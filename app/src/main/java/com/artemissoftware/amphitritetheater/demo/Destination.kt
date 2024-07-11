bc57e6d7dd1a58934fa3f9f517eed47357ff774b
gustavo <gustavo.isaac@gmail.com>
2024-07-11 10:30:29 +0100

    DemoSelectorScreen

--- app/src/main/java/com/artemissoftware/amphitritetheater/demo/Destination.kt
+++ app/src/main/java/com/artemissoftware/amphitritetheater/demo/Destination.kt
@@ -1,5 +1,12 @@
 package com.artemissoftware.amphitritetheater.demo
 
 sealed class Destination(val route: String, val title: String) {
+    data object Home : Destination(route = "home", title = "Demo")
     data object SharedElementTransition : Destination(route = "shared_element_transition", title = "Shared Element Transition")
+
+    companion object{
+        val demos = listOf(
+            SharedElementTransition
+        )
+    }
 }
