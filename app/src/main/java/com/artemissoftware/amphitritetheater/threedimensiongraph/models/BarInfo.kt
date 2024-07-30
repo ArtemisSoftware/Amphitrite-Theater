b27efce8ef0cdaf8e5871f6f55743c82e388a0f2
gustavo <gustavo.isaac@gmail.com>
2024-07-30 15:29:33 +0100

    BarInfo

---
+++ app/src/main/java/com/artemissoftware/amphitritetheater/threedimensiongraph/models/BarInfo.kt
@@ -0,0 +1,9 @@
+package com.artemissoftware.amphitritetheater.threedimensiongraph.models
+
+import androidx.compose.ui.graphics.Color
+
+data class BarInfo(
+    val value: Int,
+    val description: String,
+    val color: Color
+)
