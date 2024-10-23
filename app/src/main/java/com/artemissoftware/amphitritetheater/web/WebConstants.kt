67051f4fb777748929961c01152f57c2421f0a89
gustavo <gustavo.isaac@gmail.com>
2024-10-23 15:39:58 +0100

    Added new web constants

--- app/src/main/java/com/artemissoftware/amphitritetheater/web/WebConstants.kt
+++ app/src/main/java/com/artemissoftware/amphitritetheater/web/WebConstants.kt
@@ -4,6 +4,13 @@ internal object WebConstants {
     const val DEFAULT_URL = "https://www.google.com/"
     const val DC_URL = "https://www.dc.com"
     const val MARVEL_URL = "https://www.marvel.com"
+    const val IMAGE_URL = "https://imagecomics.com/"
+
+    const val KASA_URL = "https://www.homestory.pt"
     const val ERROR_URL = "https://www.google.com/"
     const val DEFAULT_WEB_VIEW_URL = MARVEL_URL
+
+    const val CUSTOM_WEB_VIEW_PAGE_LOCAL_URL = "file:///android_asset/custom_web_view_page.html"
+    const val REDIRECT_LOCAL_URL = "file:///android_asset/redirect.html"
+    const val POST_MESSAGE_LOCAL_URL = "file:///android_asset/post_message.html"
 }
